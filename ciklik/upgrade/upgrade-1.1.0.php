e1f21954f5e8365952779ccdc5ec7717c6f5e882
Jean-François Viguier <16720275+jf-viguier@users.noreply.github.com>
2024-04-19 10:44:59 +0200

    bug fix sur l'update car le hook n'existe pas en natif dans prestashop

--- ciklik/upgrade/upgrade-1.1.0.php
+++ ciklik/upgrade/upgrade-1.1.0.php
@@ -10,13 +10,13 @@ function upgrade_module_1_1_0($object)
 {
     $purchase_type_attributes = AdminConfigureCiklikController::getCiklikAttributes(Ciklik::CONFIG_PURCHASE_TYPE_ATTRIBUTE_GROUP_ID);
     $frequencies_attributes = AdminConfigureCiklikController::getCiklikAttributes(Ciklik::CONFIG_FREQUENCIES_ATTRIBUTE_GROUP_ID);
+    $object->registerHook('actionGetProductPropertiesBefore');
     $id_hook = Hook::getIdByName('actionGetProductPropertiesBefore', false);
 
     return (
         Configuration::updateValue(Ciklik::CONFIG_ONEOFF_ATTRIBUTE_ID, $purchase_type_attributes[0]['id_attribute']) &&
         Configuration::updateValue(Ciklik::CONFIG_SUBSCRIPTION_ATTRIBUTE_ID, $purchase_type_attributes[1]['id_attribute']) &&
         Configuration::updateValue(Ciklik::CONFIG_DEFAULT_SUBSCRIPTION_ATTRIBUTE_ID, $frequencies_attributes[0]['id_attribute']) &&
-        $object->registerHook('actionGetProductPropertiesBefore') &&
         $object->updatePosition($id_hook, 0, 1)
     );
 }
