4a59ce7612980ad895ec628ceb13b3d8874bc707
Jonathan Danse <j.danse@prestaedit.com>
2024-09-05 09:06:13 +0200

    Addons Validator : Licenses

--- src/Helpers/CartHelper.php
+++ src/Helpers/CartHelper.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author    Metrogeek SAS <support@ciklik.co>
+ * @copyright Since 2017 Metrogeek SAS
+ * @license   https://opensource.org/license/afl-3-0-php/ Academic Free License (AFL 3.0)
+ */
 
 namespace PrestaShop\Module\Ciklik\Helpers;
 
