a852c7926a0cd0a5e0e8073dbe2481fc8a3e63f8
Francois <rikless@pm.me>
2024-10-08 10:21:40 +0200

    fix(addons): ajout de la product key pour addons

--- ciklik.php
+++ ciklik.php
@@ -30,7 +30,7 @@ class Ciklik extends PaymentModule
 {
     use Account;
 
-    const VERSION = '1.3.3';
+    const VERSION = '1.3.4';
     const CONFIG_API_TOKEN = 'CIKLIK_API_TOKEN';
     const CONFIG_MODE = 'CIKLIK_MODE';
     const CONFIG_HOST = 'CIKLIK_HOST';
@@ -60,7 +60,7 @@ class Ciklik extends PaymentModule
     {
         $this->name = 'ciklik';
         $this->tab = 'payments_gateways';
-        $this->version = '1.3.3';
+        $this->version = '1.3.4';
         $this->author = 'Ciklik';
         $this->currencies = true;
         $this->currencies_mode = 'checkbox';
@@ -74,6 +74,7 @@ class Ciklik extends PaymentModule
             'external',
             'validation',
         ];
+        $this->module_key = 'ad787a7f180e5ed32bf2effd4ca36520';
 
         parent::__construct();
 
