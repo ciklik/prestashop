71c911d900ad92afe378bda256924f0765cb036a
Francois <francois@louchart.net>
2026-07-15 13:57:17 +0200

    fix(cart-footer): footer et alertes désactivés par défaut
    L'affichage du pied de panier est un choix volontaire du marchand :
    les trois interrupteurs sont posés à 0 à l'installation comme à
    l'upgrade, au lieu d'une activation automatique.

--- src/Install/Installer.php
+++ src/Install/Installer.php
@@ -96,11 +96,12 @@ class Installer
             && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_ENABLE_CREATION_ORDER_STATE, '0')
             && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CREATION_ORDER_STATE, '0')
             && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_ENABLE_SKIP_NEXT_DELIVERY, '0')
-            // Récap panier : footer + alertes activés par défaut ; messages laissés
-            // vides (le template retombe sur des textes traduisibles par défaut).
-            && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CART_FOOTER_ENABLED, '1')
-            && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CART_ALERT_MIXED_ENABLED, '1')
-            && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CART_ALERT_FREQ_ENABLED, '1');
+            // Récap panier : footer + alertes désactivés par défaut, activation
+            // volontaire en BO ; messages laissés vides (le template retombe
+            // sur des textes traduisibles par défaut).
+            && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CART_FOOTER_ENABLED, '0')
+            && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CART_ALERT_MIXED_ENABLED, '0')
+            && (bool) \Configuration::updateGlobalValue(\Ciklik::CONFIG_CART_ALERT_FREQ_ENABLED, '0');
     }
 
     /**
