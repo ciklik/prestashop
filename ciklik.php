48c57477b67b5b66f91d04a4f4e9bca276399e00
Francois <francois@louchart.net>
2026-09-23 08:09:20 +0200

    chore(release): 1.23.0

--- ciklik.php
+++ ciklik.php
@@ -39,7 +39,7 @@ class Ciklik extends PaymentModule
 {
     use Account;
 
-    const VERSION = '1.22.2';
+    const VERSION = '1.23.0';
     const CONFIG_API_TOKEN = 'CIKLIK_API_TOKEN';
     const CONFIG_MODE = 'CIKLIK_MODE';
     const CONFIG_HOST = 'CIKLIK_HOST';
@@ -100,7 +100,7 @@ class Ciklik extends PaymentModule
         // Doit rester un littéral : le validateur PrestaShop Addons lit ce champ
         // par regex et refuse toute expression non-littérale (self::VERSION, etc.).
         // À garder synchronisé avec la constante VERSION ci-dessus.
-        $this->version = '1.22.2';
+        $this->version = '1.23.0';
         $this->author = 'Ciklik';
         $this->currencies = true;
         $this->currencies_mode = 'checkbox';
